dcf4e8f9473a3412fc56f6cac65b8cf3decb6e0d
martin-arreola <martin.arreola.dev@gmail.com>
2022-10-14 12:08:59 -0600

    First Commit

---
+++ backend/base/products.py
@@ -0,0 +1,54 @@
+products = [
+  {
+    '_id': '1',
+    'name': 'CS:GO',
+    'image': '/images/CSGO.jpg',
+    'description':
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    'brand': 'Apple',
+    'category': 'Electronics',
+    'price': 89.99,
+    'countInStock': 10,
+    'rating': 4.5,
+    'numReviews': 12,
+  },
+  {
+    '_id': '2',
+    'name': 'Venge Logo',
+    'image': '/images/VengeLogoDark.png',
+    'description':
+      'Sage Main',
+    'brand': 'Apple',
+    'category': 'Electronics',
+    'price': 89.99,
+    'countInStock': 10,
+    'rating': 4.5,
+    'numReviews': 12,
+  },
+  {
+    '_id': '3',
+    'name': 'Ducknana',
+    'image': '/images/ducknana.jpg',
+    'description':
+      'this is what happens when you drink the milk in your cousins sock',
+    'brand': "Martin's toys",
+    'category': 'Unknown',
+    'price': 19.99,
+    'countInStock': 1,
+    'rating': 5,
+    'numReviews': 0,
+  },
+  {
+    '_id': '4',
+    'name': 'CypherBlue.jpg',
+    'image': '/images/VengeLogoDark.png',
+    'description':
+      'He always dies... but... we love him',
+    'brand': 'Valorant',
+    'category': 'Electronics',
+    'price': 89.99,
+    'countInStock': 10,
+    'rating': 4.5,
+    'numReviews': 12,
+  },
+]
